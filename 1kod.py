e0e6a9e571456810703471352a65742e1b97b956
Bringmecarspls <Winkler.Patryk.krk@gmail.com>
2024-04-23 09:54:00 +0200

    .

---
+++ 1kod.py
@@ -0,0 +1,14 @@
+import random
+import string
+
+print ("Hello. I'm a password generator. Please type how long should be your password.")
+x = int(input("Enter a number >>> "))
+
+haslo=[]
+
+for i in range (x):
+    string.ascii_letters
+    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()!@#$%^&*()1234567890!@#$%^&*)(-=7'
+    y = random.choice(string.ascii_letters)
+    haslo.append(y)
+print(*haslo)
