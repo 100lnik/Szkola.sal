cf0088c1ef96a4eeea96942dc8df9c2a08bab095
Bringmecarspls <Winkler.Patryk.krk@gmail.com>
2024-04-23 10:23:35 +0200

    .

---
+++ 2kod.py
@@ -0,0 +1,8 @@
+import random
+print ("Hello. I'm a pesel verification program. Please give me some information about you.")
+Y = int(input("Enter a year of birth (last 2 digits) >>> "))
+M = int(input("Enter a month of birth (as a number) >>> "))
+D = int(input("Enter a day of birth (as a number) >>> "))
+P = input("Enter a your gender on birth (male, female) >>> ")
+yourpesel = int(input(""))
+
